7e43300a33dd1f8cc86f0c6c443684555ea19358
pbardea <pbardea@gmail.com>
2014-12-19 07:35:27 -0500

    A start

---
+++ background.js
@@ -0,0 +1,31 @@
+/* Create a context-menu */
+chrome.contextMenus.create({
+    id: "myContextMenu",   // <-- mandatory with event-pages
+    title: "Click me",
+    contexts: ["all"]
+});
+
+/* Register a listener for the `onClicked` event */
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  alert("hi")
+    if (tab) {
+        /* Create the code to be injected */
+        var code = [
+            'var d = document.createElement("div");',
+            'd.setAttribute("style", "'
+                + 'background-color: black; '
+                + 'color: white;'
+                + 'fontSize: large;'
+                + 'position: fixed; '
+                + 'bottom: 20px; '
+                + 'left: 30px; '
+                + 'z-index: 9999; '
+                + '");',
+            'd.innerHTML="HELLO!";',
+            'document.body.appendChild(d);'
+        ].join("\n");
+
+        /* Inject the code into the current tab */
+        chrome.tabs.executeScript(tab.id, { code: code });
+    }
+});
---
+++ popup.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+
+
+// Run our kitten generation script as soon as the document's DOM is ready.
+//document.addEventListener('DOMContentLoaded', function () {
+//});
