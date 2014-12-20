e404f30e2cf85afcb8ebb65d037f125e6dbefcdf
pbardea <pbardea@gmail.com>
2014-12-19 22:05:35 -0500

    Made captioning function

--- background.js
+++ background.js
@@ -1,31 +1,50 @@
-/* Create a context-menu */
+function displayCaption(info, tab){
+  if (tab) {
+      //Create the code to be injected
+      var code = [
+          'var d = document.createElement("div");',
+          'd.setAttribute("style", "'
+              + 'background-color: black; '
+              + 'color: white;'
+              + 'fontSize: large;'
+              + 'position: fixed; '
+              + 'bottom: 20px; '
+              + 'left: 30px; '
+              + 'z-index: 9999; '
+              + '");',
+          'd.setAttribute("id", "caption");',
+          'd.innerHTML="Welcome to Chrome CC!";',
+          'document.body.appendChild(d);'
+      ].join("\n");
+      var movieTabId = tab.id;
+
+      //Inject the code into the current tab
+      chrome.tabs.executeScript(movieTabId, { code: code });
+    }
+}
+
+function setCaptionTo(info, tab, line){
+  if (tab){
+    var code = 'document.getElementById("caption").innerHTML ="'+line+'";';
+    chrome.tabs.executeScript(tab.id, {code: code});
+  }
+}
+
+
+
+//the following uses a context menu (ie right click menu)
+
+// Create a context-menu
 chrome.contextMenus.create({
-    id: "myContextMenu",   // <-- mandatory with event-pages
-    title: "Click me",
+    id: "enableCC",   // <-- mandatory with event-pages
+    title: "Enable CC",
     contexts: ["all"]
 });
 
-/* Register a listener for the `onClicked` event */
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
-  alert("hi")
-    if (tab) {
-        /* Create the code to be injected */
-        var code = [
-            'var d = document.createElement("div");',
-            'd.setAttribute("style", "'
-                + 'background-color: black; '
-                + 'color: white;'
-                + 'fontSize: large;'
-                + 'position: fixed; '
-                + 'bottom: 20px; '
-                + 'left: 30px; '
-                + 'z-index: 9999; '
-                + '");',
-            'd.innerHTML="HELLO!";',
-            'document.body.appendChild(d);'
-        ].join("\n");
 
-        /* Inject the code into the current tab */
-        chrome.tabs.executeScript(tab.id, { code: code });
-    }
+// Register a listener for the `onClicked` event
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  displayCaption(info, tab);
+  //setCaptionTo(info,tab,"NEW LINE PLEASE!");
+  playScript(info, tab, "file:///Users/pbardea/chromecc/example.srt");
 });
